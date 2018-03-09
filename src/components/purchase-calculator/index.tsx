2460927e01fc763c21cfbed4ce209908f9c08b83
jywkim <jkim589@gmail.com>
2018-03-09 12:32:56 -0500

    set up purchase calculator component

--- src/components/purchase-calculator/index.tsx
+++ src/components/purchase-calculator/index.tsx
@@ -5,11 +5,55 @@ interface Props {
 }
 
 interface State {
-
+  shareQuantity: number;
+  sharePrice: number;
+  subTotal: number;
 }
+
 class PurchaseCalculator extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      shareQuantity: 0,
+      sharePrice: 0,
+      subTotal: 0,
+    };
+  }
+
+  /* tslint:disable no-any */
+  handleOnChange = (e: any) => {
+    const { name, value } = e.target;
+    const { shareQuantity, sharePrice, subTotal } = Object.assign({}, this.state, { [name]: value});
+
+    let state = this.state;
+    if (name === 'shareQuantity' || name === 'sharePrice') {
+      state = {
+        shareQuantity,
+        sharePrice,
+        subTotal: shareQuantity * sharePrice,
+      };
+    } else if (name === 'subTotal') {
+      state = {
+        shareQuantity: subTotal / sharePrice,
+        sharePrice,
+        subTotal,
+      };
+    }
+
+    this.setState(state);
+  }
+
   render() {
-    return <div>HELLO WORLD</div>;
+    const { shareQuantity, sharePrice, subTotal } = this.state;
+
+    return (
+      <div>
+        <input name="shareQuantity" value={shareQuantity} onChange={this.handleOnChange}/>
+        <input name="sharePrice" value={sharePrice} onChange={this.handleOnChange}/>
+        <input name="subTotal" value={subTotal} onChange={this.handleOnChange}/>
+      </div>
+    );
   }
 }
 
